16ea822c509d416008a520b5a98841427aeb2311
fardiab <feridabd37@gmail.com>
2023-03-17 16:26:54 +0400

    fixing

--- app/main.py
+++ app/main.py
@@ -1,10 +1,14 @@
 from flask import Flask
-
+from redis import Redis
 app = Flask(__name__)
+r = Redis(host='redis', decode_responses=True, port=6379)
+
+r.set('counter', 0)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    r.incr('counter')
+    return f"You are visitor number {r.get('counter')}"
 
 if __name__ == "__main__":
     app.run(debug=True)
